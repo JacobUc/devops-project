7fc891fc2585e265fdea8feb937fd3eb4b6d233a
Alexis Rosaldo <60054142+Aler011@users.noreply.github.com>
2025-04-28 01:44:26 -0600

    feat: Added Assignments model and migration table

---
+++ devops-api/app/Models/Assignment.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Assignment extends Model
+{
+    protected $primaryKey = 'id_assignment';
+
+    protected $fillable = [
+        'assignment_date',
+        'id_driver',
+        'id_vehicle',
+    ];
+
+    // Relaciones
+    public function driver()
+    {
+        return $this->belongsTo(Driver::class, 'id_driver', 'id_driver');
+    }
+
+    public function vehicle()
+    {
+        return $this->belongsTo(Vehicle::class, 'id_vehicle', 'id_vehicle');
+    }
+}
