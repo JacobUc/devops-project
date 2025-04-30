9edddda534d43963c6839abd86e612ff6b8d89cb
Alexis Rosaldo <60054142+Aler011@users.noreply.github.com>
2025-04-29 23:53:54 -0600

    hotfix: Corrections to entrypoint.sh file for database creation.

--- devops-api/entrypoint.sh
+++ devops-api/entrypoint.sh
@@ -1,4 +1,3 @@
-
 composer install
 
 echo "Esperando a que MySQL esté disponible..."
@@ -7,5 +6,9 @@ do
   sleep 2
 done
 
+# Crear la base de datos si no existe
+mysql -h $DB_HOST -u$DB_USERNAME -p$DB_PASSWORD -e "CREATE DATABASE IF NOT EXISTS ${DB_DATABASE};"
+
+# Migraciones y ejecución de Laravel
 php artisan migrate --force
 php artisan serve --host=0.0.0.0 --port=8000
